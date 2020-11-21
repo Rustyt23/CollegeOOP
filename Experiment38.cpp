c74e7d5f2ed0701f92243e39c9714a7fc0466c36
Rustyt23 <rustytrooser@gmail.com>
2020-11-21 22:10:37 +0530

    added P38 and .py

--- Experiment38.cpp
+++ Experiment38.cpp
@@ -1,1 +1,33 @@
 //38		WAP to perform modifications with 2 object in above concept.
+#include <iostream>
+using namespace std;
+class Rectangle
+{
+    int L, B;
+    public:
+        Rectangle()
+        {
+            L=2;
+            B=2;
+        }
+        void operator++()
+        {
+            L+=3;
+            B+=3;
+        }
+        void Display()
+        {
+            cout<<"\n\tLength: "<<L;
+            cout<<"\n\tBreadth: "<<B;
+        }
+};
+int main()
+{
+   Rectangle R;
+   cout<<"\n\tLength & Breadth before increment: ";
+   R.Display();
+   ++R;
+   cout<<"\n\tLength & Breadth after increment: ";
+   R.Display();
+   return 0;
+}
